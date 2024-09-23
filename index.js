3936c2751875c74739864ab6e96984fe07f085e7
Chetan Kumar Vyas <chetankrvyas@gmail.com>
2024-09-23 10:13:26 +0530

    js

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ const bodyParser = require("body-parser");
 const app = express()
 const port = 5000
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.json())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.use('uploads/', express.static('uploads'))
+app.use(cors());
+// app.use('uploads/', express.static('uploads'))
 app.use('/api/form', require('./routes/form'))
 app.use('/api/clientform', require('./routes/clientform'))
 
